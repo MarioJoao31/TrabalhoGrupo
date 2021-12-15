9cff671145ed6c33a2e4ff2c6b70505143036872
MárioPinto01 <mario.joao31@hotmail.com>
2021-12-15 15:10:48 +0000

    primeiro commit com o basico

---
+++ main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+//Estrutura dos dados
+typedef struct 
+{int numero;
+ char nome[50];
+ int ano;
+ float media;
+} Veiculo;
